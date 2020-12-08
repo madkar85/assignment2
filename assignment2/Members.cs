938f8b56ed4f1fe15ad8cba17e33e7c9e1d6d08a
madel <madeleine.karlsson@outlook.com>
2020-12-08 14:51:49 +0100

    Added new class, methods, more code and comments.

---
+++ assignment2/Members.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment2
+{
+    class Members
+    {
+        private string name;
+        private string city;
+        private int age;
+        private string housing;
+        private string family;
+        private string pets;
+        private string occupation;
+        private string hobby;
+        private string favoriteFood;
+        private string favoriteMusic;
+
+        private string reasonForProgramming;
+
+        public Members()
+        {
+
+        }
+
+        public Members(string name)
+        {
+            this.name = name;
+        }
+
+        public Members(string name, string city, int age, string housing, string family, string pets, string occupation, string hobby, 
+            string favoriteFood, string favoriteMusic, string reasonForProgramming)
+        {
+            this.name = name;
+            this.city = city;
+            this.age = age;
+            this.housing = housing;
+            this.family = family;
+            this.pets = pets;
+            this.occupation = occupation;
+            this.hobby = hobby;
+            this.favoriteFood = favoriteFood;
+            this.favoriteMusic = favoriteMusic;
+            this.reasonForProgramming = reasonForProgramming;
+        }
+
+
+
+        public string Name { get => name; set => name = value; }
+        public string City { get => city; set => city = value; }
+        public int Age { get => age; set => age = value; }
+        public string Housing { get => housing; set => housing = value; }
+        public string Family { get => family; set => family = value; }
+        public string Pets { get => pets; set => pets = value; }
+        public string Occupation { get => occupation; set => occupation = value; }
+        public string Hobby { get => hobby; set => hobby = value; }
+        public string FavoriteFood { get => favoriteFood; set => favoriteFood = value; }
+        public string FavoriteMusic { get => favoriteMusic; set => favoriteMusic = value; }
+        public string ReasonForProgramming { get => reasonForProgramming; set => reasonForProgramming = value; }
+    }
+}
--- assignment2/Program.cs
+++ assignment2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace assignment2
 {
@@ -7,12 +8,15 @@ namespace assignment2
 
         static void Main(string[] args)
         {
-            StartPage();
+            List<string> membersOfTheGroup = new List<string> {"Madde" };
+            List<string> informationAboutEachMember = new List<string> {"bor i Haninge" };
+
+            StartPage(membersOfTheGroup, informationAboutEachMember);
         }
 
 
         //Startar programmet och ber om en kod. Vid fel kod stängs programmet
-        public static void StartPage()
+        public static void StartPage(List<string> membersOfTheGroup, List<string> informationAboutEachMember)
         {
             Console.WriteLine("Välkommen! Vänligen ange en kod för att fortsätta: ");
             string answer = Console.ReadLine();
@@ -23,7 +27,7 @@ namespace assignment2
             }
             else
             {
-                Menu();
+                Menu(membersOfTheGroup, informationAboutEachMember);
             }
 
         }
@@ -35,28 +39,86 @@ namespace assignment2
         }
 
         //Skriver ut en meny och ger tre val. Alla valen skickar vidare till olika metoder.
-        public static void Menu()
+        public static void Menu(List<string> membersOfTheGroup, List<string> informationAboutEachMember)
         {
             Console.WriteLine("Korrekt kod. Välkommen till gruppen Coffee n Code");
             Console.WriteLine("Vad vill du göra?");
             Console.WriteLine("1. Skriv ut alla personer i gruppen");
             Console.WriteLine("2. Skriv ut generell information om en person");
             Console.WriteLine("3. Ta bort en person ur listan");
-            string answer = Console.ReadLine();
+            int answer = Convert.ToInt32(Console.ReadLine());
 
             // om svaret är 1, skicka till metod som skriver ut listan med alla personer
             // om svaret är 2, skicka till metod som ber användaren välja vilken person och sedan skriver ut denna och informationen
             // om svaret är 3, skicka till metod som ber om vilken person att ta bort och sedan tar bort denna ur listan.
+            switch (answer)
+            {
+                case 1:
+                    PrintAllMembers(membersOfTheGroup);
+                    break;
+                case 2:
+                    PrintInformation(membersOfTheGroup, informationAboutEachMember);
+                    break;
+                case 3:
+                    RemovePerson(membersOfTheGroup);
+                    break;
+                default:
+                    Console.WriteLine("Fel input.");
+                    break;
+            }
         }
 
+        public static void PrintAllMembers(List<string> membersOfTheGroup)
+        {
+            //Skriver ut alla medlemmar i gruppen
+            foreach(string index in membersOfTheGroup)
+            {
+                 Console.WriteLine(index);
+            }
+
+        }
+
+        public static void PrintInformation(List<string> membersOfTheGroup, List<string> informationAboutTheGroup)
+        {
+            //Användaren får först välja vem de vill veta mer om, sedan skrivs den personens information ut.
+            Console.WriteLine("Vem vill du veta mer om?");
+
+            //skriver ut medlemmarna med sin indexsiffra, tänker att man skriver vilken siffran på personen i fråga då.
+            for(int i = 0; i<membersOfTheGroup.Count; i++)
+            {
+                Console.WriteLine(i + "." + membersOfTheGroup[i]);
+            }
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(informationAboutTheGroup[answer]);
+             
+            
+        }
+
+        public static void RemovePerson(List<string> membersOfTheGroup)
+        {
+            //Användaren väljer vem som ska tas bort
+            Console.WriteLine("Vem vill du ta bort?");
+            foreach (string index in membersOfTheGroup)
+            {
+                Console.WriteLine(index);
+            }
+            string answer = Console.ReadLine();
 
 
 
+        }
     }
 
 
+ }
+
+
+    
 
 
 
 
-}
+
+
+
